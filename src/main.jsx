7503a25ec9f466d49226656a1d8bf1cf187b6253
dibuatpekat-eng <dibuatpekat@gmail.com>
2026-03-24 13:44:51 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdminDashboard from './AdminDashboard.jsx'
+
+const isAdmin = window.location.pathname === '/admin'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {isAdmin ? <AdminDashboard /> : <App />}
+  </React.StrictMode>
 )
